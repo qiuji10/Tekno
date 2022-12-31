491d7b3f0da5d5da0411fa66611e53f7e648aa7e
qiuji <74701224+qiuji10@users.noreply.github.com>
2022-12-31 23:48:37 +0800

    Fix bug - Only trigger when colliding platform's top

--- Assets/PlayerController/PlatformPlayerPosChecker.cs
+++ Assets/PlayerController/PlatformPlayerPosChecker.cs
@@ -12,12 +12,14 @@ public class PlatformPlayerPosChecker : MonoBehaviour
 
             float playerHeight = transform.localScale.y / 2;
 
-            if (player.position.y > playerHeight && player.position.y < playerHeight + 0.1f)
-            {
-                return;
-            }
+            if (player.position.y > playerHeight && player.position.y < playerHeight + 0.1f) return;
 
-            player.position = new Vector3(player.position.x, transform.position.y + transform.localScale.y / 2, player.position.z);
+            Vector3 normal = collision.GetContact(0).normal;
+
+            if (normal.y > 0)
+            {
+                player.position = new Vector3(player.position.x, transform.position.y + transform.localScale.y / 2, player.position.z);
+            }
         }
     }
 }
