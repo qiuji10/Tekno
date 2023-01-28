376e16479ad3b526262439f00ab8a9206d03ff58
qiuji <74701224+qiuji10@users.noreply.github.com>
2023-01-28 08:15:40 +0800

    Input Gyro Testing

---
+++ Assets/3_Scripts/DualShock Gyro/UI_Gyro.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class UI_Gyro : MonoBehaviour
+{
+    [SerializeField] float space = 50;
+    [SerializeField] float sensitivity = 100;
+
+    private Vector2 minPos;
+    private Vector2 maxPos;
+    private Gamepad controller = null;
+    private RectTransform imageRect;
+
+    void Start()
+    {
+        controller = DS4.getConroller();
+        imageRect = GetComponent<RectTransform>();
+
+        minPos.x = imageRect.anchoredPosition.x - space;
+        minPos.y = imageRect.anchoredPosition.y - space;
+
+        maxPos.x = imageRect.anchoredPosition.x + space;
+        maxPos.y = imageRect.anchoredPosition.y + space;
+    }
+
+    void Update()
+    {
+        if (controller != null)
+        {
+            Vector2 newPos = imageRect.anchoredPosition;
+            Quaternion rotation = DS4.getRotation();
+            newPos.y -= rotation.x * sensitivity;
+            newPos.x += rotation.y * sensitivity;
+            newPos.x = Mathf.Clamp(newPos.x, minPos.x, maxPos.x);
+            newPos.y = Mathf.Clamp(newPos.y, minPos.y, maxPos.y);
+            imageRect.anchoredPosition = newPos;
+        }
+    }
+}
