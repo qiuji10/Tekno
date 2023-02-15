b5eda4f170bac367a20d6ee941b58aea9e7bb4de
brandonsurin28 <brandonsurin8@gmail.com>
2023-02-15 20:59:27 +0800

    Music Sync To Color Change

---
+++ Assets/3_Scripts/MusicSystem/ColorChangeSync.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SonicBloom.Koreo;
+using SonicBloom.Koreo.Players;
+
+public class ColorChangeSync : MonoBehaviour
+{
+    [EventID]
+    public string eventID;
+    public KoreographyTrack Track;
+
+    [SerializeField]
+    private Material[] materials;
+
+
+    [SerializeField]
+    private GameObject cube;
+
+    private Renderer cubeRenderer;
+
+    private int randomTextureIndex;
+    private int intValueEvt;
+
+    private void Awake()
+    {
+        cubeRenderer = cube.GetComponent<Renderer>();
+        Koreographer.Instance.RegisterForEventsWithTime(eventID, OnBeatChange);
+    }
+
+    private void OnBeatChange(KoreographyEvent evt, int sampleTime, int sampleData, DeltaSlice deltaSlice)
+    {
+        //Getting input color based on Int Range
+        intValueEvt = evt.GetIntValue();
+
+        if (intValueEvt == 0)
+        {
+            cubeRenderer.material = materials[0];
+        }
+        else if (intValueEvt == 1)
+        {
+            cubeRenderer.material = materials[1];
+        }
+        else if (intValueEvt == 2)
+        {
+            cubeRenderer.material = materials[2];
+        }
+        else if (intValueEvt == 3)
+        {
+            cubeRenderer.material = materials[3];
+        }
+    }
+}
