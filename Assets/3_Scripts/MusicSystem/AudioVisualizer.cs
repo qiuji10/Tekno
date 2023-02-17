9373784f1b440a58cb1e121d452a03cd27b551c5
brendanou <0132735@kdu-online.com>
2023-02-18 01:30:50 +0800

    bar Work Still in progress

--- Assets/3_Scripts/MusicSystem/AudioVisualizer.cs
+++ Assets/3_Scripts/MusicSystem/AudioVisualizer.cs
@@ -24,22 +24,31 @@ public class AudioVisualizer : MonoBehaviour
     //    bars = new List<GameObject>();
     //    Koreographer.Instance.RegisterForEvents(eventID, OnBeatEvent);
     //}
+
     //private void OnBeatEvent(KoreographyEvent evt)
     //{
-
+    //    // Get data from Koreography
     //    float[] frequencyData = evt.GetValueOfCurveAtTime<float[]>(0);
-    //    for (int i = 0; i < frequencyData.Length; i++)
+
+       
+    //    for (int i = 0; i < frequencyData.Length; i++)  // Create bars for each frequency range
     //    {
     //        if (i >= bars.Count)
     //        {
     //            GameObject newBar = Instantiate(barPrefab, transform);
     //            bars.Add(newBar);
     //        }
+
+    //        // Scale bar based on amplitude.
     //        float barScale = Mathf.Clamp(frequencyData[i] * barMaxScale, barMinScale, barMaxScale);
     //        bars[i].transform.localScale = new Vector3(barWidth, barScale, barWidth);
+
+    //        //Position bar and the bar distance
     //        float xPos = (i - (frequencyData.Length / 2f)) * barDistance;
     //        bars[i].transform.localPosition = new Vector3(xPos, barHeight / 2f, 0f);
     //    }
+
+    //    //Disable unused bars.
     //    for (int i = frequencyData.Length; i < bars.Count; i++)
     //    {
     //        bars[i].SetActive(false);
