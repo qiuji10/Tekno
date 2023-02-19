30d11f35c6caff9069f8c21f083b13a71935bec9
brendanOU17 <98091999+brendanOU17@users.noreply.github.com>
2023-02-19 15:56:35 +0800

    music Sync With animation speed

---
+++ Assets/3_Scripts/MusicSystem/MusicAnimationSync.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SonicBloom.Koreo;
+
+public class MusicAnimationSync : MonoBehaviour
+{
+    public Animator animator;
+
+    [EventID]
+    public string eventID;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Koreographer.Instance.RegisterForEvents(eventID, OnAnimationTrigger);
+        animator = GetComponent<Animator>();
+    }
+
+    void OnAnimationTrigger(KoreographyEvent evt)
+    {
+        float speedValue = evt.GetFloatValue();
+        animator.speed = speedValue;
+        Debug.Log("animation speed it " + animator.speed);
+        animator.Play("Bboy Hip Hop Move", 0);
+    }
+}
