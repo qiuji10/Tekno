15072d3f34bb42ed9f0fcb2e2c0fde3491f73a98
SharifZafran <0128023@kdu-online.com>
2023-03-09 03:11:29 +0800

    made AOE attack , included button input for teleporting
    just need to add VFX

--- Assets/3_Scripts/SharifScripts/Teleport.cs
+++ Assets/3_Scripts/SharifScripts/Teleport.cs
@@ -7,30 +7,52 @@ public class Teleport : MonoBehaviour
     public Transform destination; // the destination node
     public float moveSpeed = 5.0f; // the speed with which to move the player towards the destination
 
-    [SerializeField]private bool isTeleporting = false; // flag to indicate if the player is currently being teleported
+    [SerializeField] private bool isTeleporting = false; // flag to indicate if the player is currently being teleported
+    [SerializeField] private bool insideTrigger = false; // flag to indicate if the player is currently being teleported
+
+    // NEW: Define the button that triggers teleportation
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            // Start moving the player towards the destination
-            isTeleporting = true;
-            
+            Debug.Log("player is in");
+            // NEW: Check if the teleport button is pressed
+            insideTrigger = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("player is in");
+            // NEW: Check if the teleport button is pressed
+            insideTrigger = false;
         }
     }
 
     private void Start()
     {
-        if(destination == null)
+        if (destination == null)
         {
             destination = transform;
         }
     }
+
     private void FixedUpdate()
     {
+        if (insideTrigger)
+        {
+            if (Input.GetKey(KeyCode.Alpha5))
+            {
+                isTeleporting= true;
+            }
+        }
+
         // If the player is being teleported, move them towards the destination
         if (isTeleporting)
         {
+
             GameObject player = GameObject.FindGameObjectWithTag("Player");
             Rigidbody playerRigidbody = player.transform.parent.GetComponent<Rigidbody>();
             playerRigidbody.isKinematic = true;
@@ -43,11 +65,12 @@ public class Teleport : MonoBehaviour
 
             // If the player has reached the destination, stop teleporting them
             float distanceToDestination = Vector3.Distance(playerRigidbody.position, destination.position);
-            if (distanceToDestination < 2.5f)
+            if (distanceToDestination < 1.5f)
             {
                 isTeleporting = false;
                 playerRigidbody.isKinematic = false;
             }
         }
+
     }
 }
