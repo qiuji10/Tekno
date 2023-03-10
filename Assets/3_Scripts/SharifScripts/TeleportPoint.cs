1172ba037a9fe41b9c4d9351b413c58d3e4a3a37
qiuji <74701224+qiuji10@users.noreply.github.com>
2023-03-10 21:08:17 +0800

    Hook Prefab

---
+++ Assets/3_Scripts/SharifScripts/TeleportPoint.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportPoint : MonoBehaviour
+{
+    public Transform destination; // the destination node
+}
