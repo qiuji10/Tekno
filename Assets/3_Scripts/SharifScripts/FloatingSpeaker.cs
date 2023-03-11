7a0facc154bac6f8a2bc9eba90496d7b500d1f6e
qiuji <74701224+qiuji10@users.noreply.github.com>
2023-03-11 10:56:15 +0800

    Speaker Rotation and Beat Offset

--- Assets/3_Scripts/SharifScripts/FloatingSpeaker.cs
+++ Assets/3_Scripts/SharifScripts/FloatingSpeaker.cs
@@ -4,13 +4,33 @@ using UnityEngine;
 
 public class FloatingSpeaker : MonoBehaviour
 {
+    public Transform player;
     public Transform waypoint;
     public float moveSpeed = 10f;
-    public float smoothTimeXZ = 0.3f;
-    public float smoothTimeY = 0.6f;
+    public float smoothTimeXZ = 0f;
+    public float smoothTimeY = 0.2f;
+    public float beatOffsetY = 5f;
+    public float lookAtSmoothTime = 0f; // added variable for look at smoothing
+
     private Vector3 velocityXZ = Vector3.zero;
     private Vector3 velocityY = Vector3.zero;
     private Vector3 targetPosition;
+    private bool isOnBeat = false;
+
+    private void OnEnable()
+    {
+        TempoManager.OnBeat += TempoManager_OnBeat;
+    }
+
+    private void OnDisable()
+    {
+        TempoManager.OnBeat -= TempoManager_OnBeat;
+    }
+
+    private void TempoManager_OnBeat()
+    {
+        isOnBeat = true;
+    }
 
     void Update()
     {
@@ -23,6 +43,19 @@ public class FloatingSpeaker : MonoBehaviour
 
         // Move the object towards the target position on the y axis
         Vector3 targetPositionY = new Vector3(transform.position.x, targetPosition.y, transform.position.z);
+
+        if (isOnBeat)
+        {
+            targetPositionY.y -= beatOffsetY;
+            isOnBeat = false;
+        }
+
         transform.position = Vector3.SmoothDamp(transform.position, targetPositionY, ref velocityY, smoothTimeY);
+
+        // Calculate desired rotation using LookRotation
+        Quaternion targetRotation = Quaternion.LookRotation(player.position);
+
+        // Smoothly interpolate between current rotation and target rotation
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime / lookAtSmoothTime);
     }
 }
