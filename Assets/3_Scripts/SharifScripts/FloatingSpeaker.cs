337b4b93132ecc466dca70309149098275b1537f
qiuji <74701224+qiuji10@users.noreply.github.com>
2023-03-11 10:29:51 +0800

    Naming Convention

--- Assets/3_Scripts/SharifScripts/FloatingSpeaker.cs
+++ Assets/3_Scripts/SharifScripts/FloatingSpeaker.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class FloatingSpeaker : MonoBehaviour
 {
-    public GameObject player;
+    public Transform waypoint;
     public float moveSpeed = 10f;
     public float smoothTimeXZ = 0.3f;
     public float smoothTimeY = 0.6f;
@@ -15,7 +15,7 @@ public class FloatingSpeaker : MonoBehaviour
     void Update()
     {
         // Set target position for movement
-        targetPosition = player.transform.position;
+        targetPosition = waypoint.position;
 
         // Move the object towards the target position on the x and z axes
         Vector3 targetPositionXZ = new Vector3(targetPosition.x, transform.position.y, targetPosition.z);
