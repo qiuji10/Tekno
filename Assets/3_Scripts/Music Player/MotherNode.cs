c044f495ad663e68b9e98eb0a5458f2dda76a6f5
SharifZafran <sharifzafran@gmail.com>
2023-04-24 03:18:46 +0800

    combine node script/modification

--- Assets/3_Scripts/Music Player/MotherNode.cs
+++ Assets/3_Scripts/Music Player/MotherNode.cs
@@ -4,29 +4,42 @@ using UnityEngine;
 
 public class MotherNode : MonoBehaviour
 {
-    [SerializeField] public List<TeleportNode> teleportNodes = new List<TeleportNode>();
+    [SerializeField] public List<Transform> teleportPoints = new List<Transform>();
     [SerializeField] private TeleportAbility teleportAbility;
     private int currentNodeIndex = 0;
+    private bool playerInRange = false;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            if (teleportNodes.Count > 0)
-            {
-                StartCoroutine(TeleportToNodes());
-            }
+            playerInRange = true;
+            teleportAbility.motherNode = this;
         }
     }
 
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+
+    public void canTeleport()
+    {
+        if(playerInRange &&  teleportAbility != null)
+        {
+            StartCoroutine(TeleportToNodes());
+        }
+    }
     public IEnumerator TeleportToNodes()
     {
-        foreach (TeleportNode node in teleportNodes)
+        foreach (Transform teleportPoint in teleportPoints)
         {
-            if (node != null && node.gameObject.activeInHierarchy)
+            if (teleportPoint != null && teleportPoint.gameObject.activeInHierarchy)
             {
-                Transform targetTeleportPoint = (node.nextTeleportPoint != null) ? node.nextTeleportPoint : node.prevTeleportPoint;
-                teleportAbility.TeleportToNextNode(targetTeleportPoint);
+                teleportAbility.TeleportToNextNode(teleportPoint);
                 yield return new WaitForSeconds(0.15f); // wait for the player to finish teleporting before teleporting again
             }
         }
