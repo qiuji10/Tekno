235faa8a8d98aa6a1f7ef5397b854407aab1ef4e
brendanou <0132735@kdu-online.com>
2023-07-29 18:35:44 +0800

    updated the MusicSync script
    able to freely use position rotation and scale for props react to music handling

--- Assets/3_Scripts/MusicSystem/MusicSync.cs
+++ Assets/3_Scripts/MusicSystem/MusicSync.cs
@@ -18,12 +18,14 @@ public class MusicSync : MonoBehaviour
     public Material electronicMaterial;
 
     private int currentBeat;
+    private int currentSequenceIndex = 0;
 
     private Vector3 scale = Vector3.one;
     private float speed; // movement speed 
     [Header("How Do you want your Gameobject React To The Music")]
     public bool ScaleSelection;
     public bool RotationSelection;
+    public bool MoveSelection;
     [Header("Scale Settings")]
     public bool ScaleX;
     public bool ScaleY;
@@ -35,6 +37,17 @@ public class MusicSync : MonoBehaviour
     public Vector3 scaleMod = Vector3.one;
     private Vector3 originalScale;
     private Quaternion originalRotation;
+
+    [Header("Movement Options")]
+    public bool MoveX;
+    public bool MoveY;
+    public bool MoveZ;
+    public Vector3 moveMod = Vector3.one;
+    private Vector3 originalPosition;
+
+    private int currentScaleIndex = 0;
+    private int currentRotationIndex = 0;
+    private int currentMoveIndex = 0;
 
     private void OnEnable()
     {
@@ -55,6 +68,7 @@ public class MusicSync : MonoBehaviour
         // Save the original scale and rotation for resetting later
         originalScale = transform.localScale;
         originalRotation = transform.localRotation;
+        originalPosition = transform.position;
     }
 
     private void StanceManager_OnStanceChange(Track obj)
@@ -97,24 +111,64 @@ public class MusicSync : MonoBehaviour
             ChangeMaterial(intValueEvt);
             currentBeat = intValueEvt;
 
-
-            if (ScaleSelection)
+            // Apply scaling
+            if (ScaleSelection && currentScaleIndex < sequence.Length)
             {
                 Vector3 scale = originalScale;
                 scale.x = ScaleX ? evt.GetValueOfCurveAtTime(sampleTime) * 2.0f * scaleMod.x : scale.x;
                 scale.y = ScaleY ? evt.GetValueOfCurveAtTime(sampleTime) * 2.0f * scaleMod.y : scale.y;
                 scale.z = ScaleZ ? evt.GetValueOfCurveAtTime(sampleTime) * 2.0f * scaleMod.z : scale.z;
-                transform.localScale = scale;
+                sequence[currentScaleIndex].transform.localScale = scale;
+
+                // Move to the next sequence index if the current one has been fully applied
+                if (currentScaleIndex < sequence.Length - 1 && Mathf.Approximately(evt.GetValueOfCurveAtTime(sampleTime), 1.0f))
+                {
+                    currentScaleIndex++;
+                }
             }
 
-            if (RotationSelection)
+            // Apply rotation
+            if (RotationSelection && currentRotationIndex < sequence.Length)
             {
                 Quaternion rotation = originalRotation;
                 float x = RotateX ? evt.GetValueOfCurveAtTime(sampleTime) * 360.0f : 0.0f;
                 float y = RotateY ? evt.GetValueOfCurveAtTime(sampleTime) * 360.0f : 0.0f;
                 float z = RotateZ ? evt.GetValueOfCurveAtTime(sampleTime) * 360.0f : 0.0f;
                 rotation = Quaternion.Euler(x, y, z);
-                transform.localRotation = rotation;
+                sequence[currentRotationIndex].transform.localRotation = rotation;
+
+                // Move to the next sequence index if the current one has been fully applied
+                if (currentRotationIndex < sequence.Length - 1 && Mathf.Approximately(evt.GetValueOfCurveAtTime(sampleTime), 1.0f))
+                {
+                    currentRotationIndex++;
+                }
+            }
+
+            // Apply movement
+            if (MoveSelection && currentMoveIndex < sequence.Length)
+            {
+                Vector3 position = originalPosition;
+                if (MoveX)
+                {
+                    position.x = originalPosition.x + evt.GetValueOfCurveAtTime(sampleTime) * moveMod.x;
+                }
+
+                if (MoveY)
+                {
+                    position.y = originalPosition.y + evt.GetValueOfCurveAtTime(sampleTime) * moveMod.y;
+                }
+
+                if (MoveZ)
+                {
+                    position.z = originalPosition.z + evt.GetValueOfCurveAtTime(sampleTime) * moveMod.z;
+                }
+                sequence[currentMoveIndex].transform.position = position;
+
+                // Move to the next sequence index if the current one has been fully applied
+                if (currentMoveIndex < sequence.Length - 1 && Mathf.Approximately(evt.GetValueOfCurveAtTime(sampleTime), 1.0f))
+                {
+                    currentMoveIndex++;
+                }
             }
         }
     }
