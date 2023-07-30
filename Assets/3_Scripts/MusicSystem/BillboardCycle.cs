d7575e22da360a55afb5595d04c7b8b3a5033b8c
Brandon James Surin <brandonsurin8@gmail.com>
2023-07-30 16:52:16 +0800

    Billboard stuff

--- Assets/3_Scripts/MusicSystem/BillboardCycle.cs
+++ Assets/3_Scripts/MusicSystem/BillboardCycle.cs
@@ -12,14 +12,12 @@ public class BillboardCycle : MonoBehaviour
     public Material[] propagandaMaterials; // Array of materials for normal behavior
     public Material[] hijackedMaterials; // Array of materials for hijacked behavior
 
-    private Track track;
-    public static Track currentTrack;
-
     public bool isHijackedSuccessful = false;
 
     public int materialIndex = 0; // Index of the material to change (set in the inspector)
 
-    private int currentMaterialIndex = 0; // To track the current material index used
+    private Track track;
+    public static Track currentTrack;
 
     private void Awake()
     {
@@ -64,28 +62,41 @@ public class BillboardCycle : MonoBehaviour
     {
         if (isHijackedSuccessful)
         {
-            SetMaterial(currentMaterialIndex, hijackedMaterials);
+            SetRandomMaterial(hijackedMaterials);
         }
         else
         {
-            SetMaterial(currentMaterialIndex, propagandaMaterials);
+            SetRandomMaterial(propagandaMaterials);
         }
-
-        // Increment the currentMaterialIndex and loop back to the start if necessary
-        currentMaterialIndex = (currentMaterialIndex + 1) % Mathf.Max(hijackedMaterials.Length, propagandaMaterials.Length);
     }
 
-    private void SetMaterial(int index, Material[] materials)
+    private void SetRandomMaterial(Material[] materials)
     {
-        if (index >= 0 && index < materials.Length)
+        if (materials != null && materials.Length > 0)
         {
-            Renderer renderer = GetComponent<Renderer>();
-            Material[] rendererMaterials = renderer.materials;
-            if (materialIndex < rendererMaterials.Length)
+            List<Renderer> childRenderers = new List<Renderer>();
+            GetChildRenderers(transform, childRenderers);
+
+            foreach (Renderer renderer in childRenderers)
             {
-                rendererMaterials[materialIndex] = materials[index];
-                renderer.materials = rendererMaterials;
+                if (materialIndex < renderer.sharedMaterials.Length)
+                {
+                    int randomIndex = Random.Range(0, materials.Length);
+                    Material[] newMaterials = renderer.sharedMaterials;
+                    newMaterials[materialIndex] = materials[randomIndex];
+                    renderer.sharedMaterials = newMaterials;
+                }
             }
+        }
+    }
+
+    private void GetChildRenderers(Transform parent, List<Renderer> renderers)
+    {
+        renderers.AddRange(parent.GetComponents<Renderer>());
+
+        for (int i = 0; i < parent.childCount; i++)
+        {
+            GetChildRenderers(parent.GetChild(i), renderers);
         }
     }
 
