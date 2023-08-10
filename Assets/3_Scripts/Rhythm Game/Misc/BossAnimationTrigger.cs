6d870624b8e0ad6a95de7977292488435656ad2a
qiuji <74701224+qiuji10@users.noreply.github.com>
2023-08-11 00:48:06 +0800

    update animation settings

--- Assets/3_Scripts/Rhythm Game/Misc/BossAnimationTrigger.cs
+++ Assets/3_Scripts/Rhythm Game/Misc/BossAnimationTrigger.cs
@@ -1,6 +1,8 @@
+using NaughtyAttributes;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public enum Hand { Left, Right }
 
@@ -12,11 +14,49 @@ public class BossAnimationTrigger : MonoBehaviour
     [Header("References")]
     [SerializeField] private Animator anim;
 
-    private const int leftLayer = 1;
-    private const int rightLayer = 2;
+    private const int LeftLayer = 1;
+    private const int RightLayer = 2;
+    private const float TapLeft = 0, TapBoth = 0.5f, TapRight = 1;
+
+    private int BlendTap, Tap, ChargeLeft, ChargeRight, HoldLeft, HoldRight;
+
+    private void Awake()
+    {
+        BlendTap = Animator.StringToHash("BlendTap");
+        Tap = Animator.StringToHash("Tap");
+        ChargeLeft = Animator.StringToHash("ChargeLeft");
+        ChargeRight = Animator.StringToHash("ChargeRight");
+        HoldLeft = Animator.StringToHash("HoldLeft");
+        HoldRight = Animator.StringToHash("HoldRight");
+    }
 
     private void OnTriggerEnter(Collider col)
     {
         
+    }
+
+    [Button]
+    private void TapLeft_Animation()
+    {
+        anim.SetLayerWeight(LeftLayer, 0);
+        anim.SetFloat(BlendTap, TapLeft);
+        anim.SetTrigger(Tap);
+    }
+
+    [Button]
+    private void TapRight_Animation()
+    {
+        anim.SetLayerWeight(RightLayer, 0);
+        anim.SetFloat(BlendTap, TapRight);
+        anim.SetTrigger(Tap);
+    }
+
+    [Button]
+    private void TapBoth_Animation()
+    {
+        anim.SetLayerWeight(LeftLayer, 0);
+        anim.SetLayerWeight(RightLayer, 0);
+        anim.SetFloat(BlendTap, TapBoth);
+        anim.SetTrigger(Tap);
     }
 }
