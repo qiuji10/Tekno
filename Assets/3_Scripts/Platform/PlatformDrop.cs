76712a67dad60704b82b4f3d53b4969f9b0e6f44
brendanou <0132735@kdu-online.com>
2023-09-10 12:23:15 +0800

    drop platform fixed, tutorial lines added

--- Assets/3_Scripts/Platform/PlatformDrop.cs
+++ Assets/3_Scripts/Platform/PlatformDrop.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-public class PlatformDrop: MonoBehaviour,IPlatform
+public class PlatformDrop : MonoBehaviour, IPlatform
 {
     [Header("Shake and Drop Settings")]
     //public bool playerOnPlatform = false;
@@ -17,7 +17,7 @@ public class PlatformDrop: MonoBehaviour,IPlatform
     private Rigidbody rb;
     private Collider platformCollider;
     public Transform player { get; set; }
-    public bool PlayerOnPlatform {get; set;}
+    public bool PlayerOnPlatform { get; set; }
 
     private void Awake()
     {
@@ -29,35 +29,28 @@ public class PlatformDrop: MonoBehaviour,IPlatform
 
     void Update()
     {
-        DropPlatform();
+      
+ if (PlayerOnPlatform)
+        {
+            StartCoroutine(ShakeAndDropPlatform());
+        }
+      
+      
     }
 
 
     public void DropPlatform()
     {
-        if (PlayerOnPlatform)
-        {
-            shakeTimer += Time.deltaTime;
-
-            // modify the y shake to 0 to prevent player cant jump
-            Vector3 randomOffset = Random.insideUnitSphere * 0.1f;
-            randomOffset.y = 0;
-            transform.position = transform.position + new Vector3(randomOffset.x, 0, randomOffset.z);
-
-            if (shakeTimer >= shakeDuration)
+            isDropping = true;
+            transform.position += new Vector3(0, -1, 0);
+            platformCollider.enabled = false;
+            PlayerOnPlatform = false;
+            parental.SetActive(false);
+            if (player != null)
             {
-                isDropping = true;
-                transform.position += new Vector3(0, -1, 0);
-                platformCollider.enabled = false;
-                PlayerOnPlatform = false;
-                parental.SetActive(false);
-                if (player != null)
-                {
-                    player.parent = null;
-                }
+                player.parent = null;
             }
-        }
-
+        
         if (isDropping && transform.position.y < originalY - 20)
         {
             if (shakeTimer != 0) shakeTimer = 0;
@@ -65,6 +58,7 @@ public class PlatformDrop: MonoBehaviour,IPlatform
         }
         else if (!isDropping && transform.position.y == originalY)
         {
+
             isDropping = true;
         }
 
@@ -76,6 +70,17 @@ public class PlatformDrop: MonoBehaviour,IPlatform
             transform.position = new Vector3(transform.position.x, originalY, transform.position.z);
             shakeTimer = 0;
         }
+    }
+
+
+    private IEnumerator ShakeAndDropPlatform() 
+    {
+            // modify the y shake to 0 to prevent player cant jump
+            Vector3 randomOffset = Random.insideUnitSphere * 0.1f;
+            randomOffset.y = 0;
+            transform.position = transform.position + new Vector3(randomOffset.x, 0, randomOffset.z);
+           yield return new WaitForSeconds(shakeDuration);
+            DropPlatform();
     }
 }
 
