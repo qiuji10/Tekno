1f6bb1a9d183699a5e693912bbaf77dc3d4e09d2
quurkyhongyu <74701224+qiuji10@users.noreply.github.com>
2024-07-31 13:17:11 +0800

    fix

--- Assets/3_Scripts/Settings System/PauseMenu.cs
+++ Assets/3_Scripts/Settings System/PauseMenu.cs
@@ -125,8 +125,6 @@ public class PauseMenu : MonoBehaviour
 
     private IEnumerator BackToLobby_Coroutine()
     {
-        Destroy(FindObjectOfType<PlayerController>().gameObject);
-
         BehaviourTreeOwner[] enemies = FindObjectsOfType<BehaviourTreeOwner>();
 
         foreach (BehaviourTreeOwner item in enemies)
